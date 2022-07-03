cc4597470b733e4a318161f4413c98ce44795ab0
Joseph Jones <darkdescendant@hotmail.com>
2022-07-03 11:37:18 -1000

    Add identifier matching

---
+++ ParserCombinatorTests/IdentifierTests.cs
@@ -0,0 +1,55 @@
+﻿using ParserCombinators;
+using System.Text;
+
+namespace ParserCombinatorTests
+{
+    internal class IdentifierTests : ParserTestsBase
+    {
+        [Test]
+        public void MatchStringLiteral()
+        {
+            string ident = "thisIs_An_1dentifi3er";
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(ident)))
+            {
+                ps.stream = ms;
+                var parser = new Identifier();
+                var rps = parser.Apply(ps);
+                Assert.IsFalse(rps.isError);
+                Assert.That((rps as ParserState<string>)?.data, Is.EqualTo(ident));
+            }
+
+        }
+
+        [Test]
+        public void MatchStringLiteralLeadingUnderscore()
+        {
+            string ident = "_thisIs_An_1dentifi3er";
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(ident)))
+            {
+                ps.stream = ms;
+                var parser = new Identifier();
+                var rps = parser.Apply(ps);
+                Assert.IsFalse(rps.isError);
+                Assert.That((rps as ParserState<string>)?.data, Is.EqualTo(ident));
+            }
+
+        }
+
+        [Test]
+        public void MatchStringLiteralWithTrailingCharacters()
+        {
+            string ident = "_thisIs_An_1dentifi3er";
+            string testIden = $"{ident}:_";
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(testIden)))
+            {
+                ps.stream = ms;
+                var parser = new Identifier();
+                var rps = parser.Apply(ps);
+                Assert.IsFalse(rps.isError);
+                Assert.That((rps as ParserState<string>)?.data, Is.EqualTo(ident));
+            }
+
+        }
+
+    }
+}
---
+++ ParserCombinators/Identifier.cs
@@ -0,0 +1,63 @@
+﻿
+namespace ParserCombinators
+{
+    public class Identifier : Parser
+    {
+        Sequence matchParser = new Sequence(
+           new Choice(new IsChar(), new Lit('_')),
+           new Many(new Choice(new IsChar(), new IsNum(), new Lit('_'))));
+
+        public Identifier()
+        {
+        }
+
+        public Identifier(Func<ParserState, ParserState> f = null) : base(f)
+        {
+        }
+
+        string CollapseParserState(ParserState<char> ps)
+        {
+            return ps.data.ToString();
+        }
+
+        string CollapseParserState(List<ParserState> lps)
+        {
+            var ret = "";
+            foreach (var ps in lps)
+            {
+                if (ps is ParserState<char>)
+                {
+                    ret += CollapseParserState(ps as ParserState<char>);
+                }
+                else if (ps is ParserState<List<ParserState>>)
+                {
+                    ret += CollapseParserState((ps as ParserState<List<ParserState>>)?.data);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Unknown parser state collapsing identifier");
+                }
+            }
+
+            return ret;
+        }
+
+        protected override ParserState ParseFunc()
+        {
+            var currIndex = this.parserState.index;
+            var newParserState = matchParser.Apply(this.parserState);
+            if (newParserState.isError)
+            {
+                return this.parserState.ToError("Identifier: No match");
+            }
+
+            var lps = newParserState as ParserState<List<ParserState>>;
+            if (lps == null)
+                return this.parserState.ToError("Identifier: Matched but unexpeced return");
+
+            var ident = CollapseParserState(lps.data);
+
+            return ParserState<string>.WithData(newParserState, newParserState.index, ident);
+        }
+    }
+}
