974c142c8e351a84bdc482175f8c191ede590c05
Joseph Jones <darkdescendant@hotmail.com>
2022-07-03 08:39:35 -1000

    Add string literal.

---
+++ ParserCombinatorTests/StringLiteralTests.cs
@@ -0,0 +1,36 @@
+﻿using ParserCombinators;
+using System.Text;
+
+namespace ParserCombinatorTests
+{
+    internal class StringLiteralTests : ParserTestsBase
+    {
+        [Test]
+        public void MatchStringLiteral()
+        {
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes("Test")))
+            {
+                ps.stream = ms;
+                var parser = new StringLit("Test");
+                var rps = parser.Apply(ps);
+                Assert.IsFalse(rps.isError);
+                Assert.That((rps as ParserState<string>)?.data, Is.EqualTo("Test"));
+            }
+ 
+        }
+
+        [Test]
+        public void FailMatchStringLiteral()
+        {
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes("Rest")))
+            {
+                ps.stream = ms;
+                var parser = new StringLit("Test");
+                var rps = parser.Apply(ps);
+                Assert.IsTrue(rps.isError);
+                Assert.That(rps.index, Is.EqualTo(0));
+            }
+ 
+        }
+     }
+}
---
+++ ParserCombinators/StringLit.cs
@@ -0,0 +1,52 @@
+﻿
+namespace ParserCombinators
+{
+    public class StringLit : Parser
+    {
+
+        Sequence matchParser;
+
+        private void BuildParserForString(string toMatch)
+        {
+            List<Parser> litParsers = new List<Parser>();
+
+            foreach (char c in toMatch)
+            {
+                litParsers.Add(new Lit(c));
+            }
+
+            matchParser = new Sequence(litParsers);
+        }
+
+        public StringLit(string toMatch)
+        {
+            BuildParserForString(toMatch);
+        }
+
+        public StringLit(string toMatch, Func<ParserState, ParserState> f = null) : base(f)
+        {
+            BuildParserForString(toMatch);
+        }
+
+        protected override ParserState ParseFunc()
+        {
+            var newParserState = matchParser.Apply(this.parserState);
+            if (newParserState.isError)
+                return newParserState;
+
+            string matchString = "";
+            var lps = (newParserState as ParserState<List<ParserState>>)?.data;
+            if (lps == null)
+                return newParserState.ToError("StringLit: Failed to convert matched parser state to string");
+
+            foreach (var ps in lps)
+            {
+                var cps = ps as ParserState<char>;
+                if (cps != null)
+                    matchString += cps.data;
+            }
+
+            return ParserState<string>.WithData(newParserState, newParserState.index, matchString);
+        }
+    }
+}
